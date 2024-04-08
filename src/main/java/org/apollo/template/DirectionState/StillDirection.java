3bc71acc38a1116b9abb0a954175a185acc29c18
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-04-08 20:32:07 +0200

    StillDirection refactored

--- src/main/java/org/apollo/template/DirectionState/StillDirection.java
+++ src/main/java/org/apollo/template/DirectionState/StillDirection.java
@@ -5,6 +5,13 @@ import org.apollo.template.Service.Debugger.DebugMessage;
 import org.apollo.template.model.Snake;
 
 public class StillDirection implements Directionable{
+
+    /**
+     * Method for changing the snake's direction based on the pressed key code - either up, down, left or right direction.
+     *
+     * @param snake The snake object whose direction will be changed.
+     * @param keyCode The key code representing the direction change.
+     */
     @Override
     public void change(Snake snake, KeyCode keyCode) {
         if (keyCode.equals(KeyCode.UP)){
@@ -23,7 +30,6 @@ public class StillDirection implements Directionable{
             snake.setDirectionable(new RightDirection());
             DebugMessage.info(this, "Right direction");
         }
-
     }
 
 
