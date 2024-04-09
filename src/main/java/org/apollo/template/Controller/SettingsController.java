61d83f6bae09d9d1b93f1b3268dfff1e5ef52673
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-04-09 13:01:23 +0200

    music now working :D

--- src/main/java/org/apollo/template/Controller/SettingsController.java
+++ src/main/java/org/apollo/template/Controller/SettingsController.java
@@ -5,11 +5,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import org.apollo.template.Service.Alert.Alert;
+import org.apollo.template.Service.Alert.AlertComp;
+import org.apollo.template.Service.Alert.AlertType;
 import org.apollo.template.View.BorderPaneRegion;
 import org.apollo.template.View.ViewList;
 
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 
@@ -20,7 +24,7 @@ public class SettingsController implements Initializable {
     private MediaPlayer musicMediaPlayer, soundEffectPlayer;
     private final Media MAIN_MUSIC_TRACK = new Media(new File("src/main/resources/org/apollo/template/audio/TetrisSoundTrack.mp3").toURI().toString());
     private double musicSliderVolume = .5;
-    private double SoundEffectSliderVolume = .5;
+    private double soundEffectSliderVolume = .5;
 
 
     private SettingsController(){
@@ -30,11 +34,6 @@ public class SettingsController implements Initializable {
         musicMediaPlayer.setVolume(musicSliderVolume);
         musicMediaPlayer.setAutoPlay(true);
         musicMediaPlayer.play();
-
-        // setting up the sound effect
-
-
-
 
     }
 
@@ -52,6 +51,8 @@ public class SettingsController implements Initializable {
         musicSliderVolume = musicSlider.valueProperty().getValue();
         System.out.println(musicSliderVolume);
         musicMediaPlayer.setVolume(musicSliderVolume);
+
+        MainController.getInstance().addAlert(new AlertComp(AlertType.SUCCESS, "Settings has been updated!"));
 
         MainController.getInstance().changeView(ViewList.MENU, BorderPaneRegion.CENTER);
     }
